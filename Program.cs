d1cf15a9383390fc22c08e49c3a40dda3a84a88c
TeymurDevv <transmissiyaservis@gmail.com>
2024-09-13 14:59:49 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using GenericPractice.Domain.Models;
+Book book1 = new("Test1", 18, "Test3", 400, 3);
+book1.Sell();
+book1.Sell();
+book1.ShowInfo();
+Book book2 = new("Test2", 15, "Test4", 700, 1);
+book2.Sell();
+book2.ShowInfo();
+Book.Income();
+
+
+
